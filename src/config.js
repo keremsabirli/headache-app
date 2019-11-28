0cc4b81261f75611a56cec0608278bf28f25785f
keremsabirli <keremsabirli15@gmail.com>
2019-11-28 16:58:33 +0300

    somethings

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARqhPMb6TF1mMHZy0yMWIR9i58KB3S9co",
+    authDomain: "headache-db.firebaseapp.com",
+    databaseURL: "https://headache-db.firebaseio.com",
+    projectId: "headache-db",
+    storageBucket: "headache-db.appspot.com",
+    messagingSenderId: "628076735772",
+    appId: "1:628076735772:web:9f4bc6bd6f7ca642ba1ed9",
+    measurementId: "G-BJWWEGWNBS"
+  };
